c86fcc4b957fde5b9a65ed81336c7d0013b0533c
Darren <darren@darrenwhitlen.com>
2019-06-26 20:17:23 +0100

    Disable the /dice command

--- static/plugins/freenode/freenode.js
+++ static/plugins/freenode/freenode.js
@@ -7,4 +7,13 @@ kiwi.plugin('freenode', function(kiwi, log) {
         document.head.appendChild(link);
         console.debug('added');
     });
+
+    // Disable the /dice command
+    kiwi.on('input.command.dice', function(e){
+        e.handled = true;
+        var net = kiwi.state.getActiveNetwork();
+        if (net) {
+            net.ircClient.raw(e.command + ' ' + e.params);
+        }
+    });
 });
